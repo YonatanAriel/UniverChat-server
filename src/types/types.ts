aa185fb2abdc459b8df3e87184f521231c35aa90
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-30 01:27:41 +0200

    adding MessageRow type

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,13 @@ export type DateData = {
   hours: number;
   minutes: number;
 };
+
+export type MessageRow = {
+  id: number;
+  chat_room_id: number;
+  user_id: number;
+  msg_text: string;
+  photo_url?: string | null;
+  user_photo?: string | null;
+  timestamp: string;
+};
